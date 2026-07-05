05d733b3b77ece9b8d1abeaa1192ab1435c35b1c
Vaughan Kitchen <contact@tophat.nz>
2026-07-05 20:14:39 +1200

    Now a website

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use askama::Template;
+use axum::{
+    extract::Query,
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use binrw::{BinRead, binread, BinWrite, binwrite};
-use std::io;
+use serde::Deserialize;
 use std::io::Cursor;
 use std::io::prelude::*;
 use std::os::unix::net::UnixStream;
 
 #[binwrite]
 #[bw(little)]
-struct Request {
+struct SearchRequest {
     version: u8,
     command: u8,
     no_results: u16,
@@ -21,7 +29,7 @@ struct Request {
 #[allow(unused)]
 #[binread]
 #[br(little)]
-struct Result {
+struct SearchResult {
     docid_len: u16,
     #[br(count = docid_len, try_map = |bytes: Vec<u8>| String::from_utf8(bytes))]
     docid: String,
@@ -38,30 +46,24 @@ struct Result {
 #[allow(unused)]
 #[binread]
 #[br(little)]
-struct Response {
+struct SearchResponse {
     version: u8,
     command: u8,
     total_results: u16,
     no_results: u16,
     #[br(count = no_results)]
-    results: Vec<Result>,
+    results: Vec<SearchResult>,
 }
 
-fn main() -> binrw::BinResult<()> {
+fn search(query: String) -> Result<SearchResponse, binrw::Error> {
     let mut stream = UnixStream::connect("/tmp/cocomel.sock")?;
 
-    print!("Query> ");
-    io::stdout().flush().unwrap();
-
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-
-    let req = Request {
+    let req = SearchRequest {
         version: 0,
         command: 1, // search
         no_results: 10,
         offset: 0,
-        query: input,
+        query: query,
     };
 
     let mut send_buf = Cursor::new(Vec::new());
@@ -74,15 +76,77 @@ fn main() -> binrw::BinResult<()> {
     stream.read_to_end(&mut recv_buf)?;
 
     let mut recv_cursor = Cursor::new(recv_buf);
-    let response = Response::read(&mut recv_cursor)?;
+    Ok(SearchResponse::read(&mut recv_cursor)?)
+}
 
-    println!("Showing {} of {}", response.no_results, response.total_results);
-    println!();
-    for res in &response.results {
-        println!("{}", res.docid);
-        println!("{}", res.snippet);
-        println!();
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root_handler))
+        .route("/search", get(search_handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root_handler() -> Html<&'static str> {
+    Html(INDEX_HTML)
+}
+
+#[derive(Deserialize)]
+struct Params {
+    q: String,
+}
+
+#[derive(Template)]
+#[template(path = "results.html")]
+struct ResultsTemplate {
+    total_results: u16,
+    no_results: u16,
+    results: Vec<SearchResult>,
+}
+
+async fn search_handler(Query(params): Query<Params>) -> impl IntoResponse {
+    let search_results = search(params.q).unwrap();
+    let template = ResultsTemplate{
+        total_results: search_results.total_results,
+        no_results: search_results.no_results,
+        results: search_results.results,
+    };
+    HtmlTemplate(template)
+}
+
+static INDEX_HTML: &str = r#"
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>smol</title>
+  </head>
+  <body>
+    <main>
+      <h1>smol</h1>
+      <form action="/search" method="get">
+        <input type="text" name="q" placeholder="Search...">
+        <input type="submit" value="Search">
+      </form>
+    </main>
+  </body>
+</html>
+"#;
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T> where T: Template, {
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {err}"),
+            )
+            .into_response(),
+        }
     }
-
-    Ok(())
 }
